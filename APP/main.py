5d1b15fb98ec0ad28e5068cf0c3397d75886ca57
chuangli <43231436+chuangli1@users.noreply.github.com>
2020-11-23 19:02:49 +0800

    new main.py

--- APP/main.py
+++ APP/main.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
-
 from flask_cors import CORS
-app = Flask(__name__,
+
+app = Flask(__name__, 
            template_folder= "../Web_UI/dist",
            static_folder= "../Web_UI/dist",
            static_url_path='')
