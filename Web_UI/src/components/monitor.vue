c62751501e540fbf0288e7a9a20ee25180b1f5c7
chuangli <43231436+chuangli1@users.noreply.github.com>
2020-12-10 21:04:42 +0800

    add stage control

--- Web_UI/src/components/monitor.vue
+++ Web_UI/src/components/monitor.vue
@@ -2,27 +2,33 @@
 <div class = "row">
    <div class = "col-sm-9">   
        <img src = 'http://localhost:5000/video' width="640px" height="360px">
-       <el-card style="width: 640px">
+       <el-card style="width: 640px;height:230px">
            <div slot="header" style="margin-left:6px; width: 640px">
                <span><i style="padding-right:6px" class="el-icon-s-tools"></i>位移台控制</span>
            </div>
            <div class="stageCard">
-               <div class="move"><i  class="el-icon-caret-top"></i></div>
+               <div class="movetop"><i  class="el-icon-caret-top"></i></div>
                <span>
-                    <div style="display:inline" class="move"><i  class="el-icon-caret-left"></i></div>
-                    <div style="display:inline">
-                        <el-button type="info" style="width:30px; heigth:30px">
+                    <div style="display:inline" class="moveleft"><i  class="el-icon-caret-left"></i></div>
+                    <div style="display:inline" class="fBtn">
+                        <el-button type="info" style="width:30px;">
                             <span style="position:relative; left:-4px; boarder:none">F</span>
                             </el-button>
                         </div>
-                    <div style="display:inline" class="move"><i  class="el-icon-caret-right"></i></div>
+                    <div style="display:inline" class="moveright"><i  class="el-icon-caret-right"></i></div>
                </span>
-               <div class="move"><i  class="el-icon-caret-bottom"></i></div>
+               <div class="movebottom"><i  class="el-icon-caret-bottom"></i></div>
            </div>
+           <div class="general" style="float:right;position:relative; bottom:130px;right:250px; height:140px; width:100px;">
+                <span class="title">Z轴移动</span>
+                <div style="margin:10px"><el-button type="info" icon="el-icon-top"></el-button></div>
+                <div style="margin:10px"><el-button type="info" icon="el-icon-bottom"></el-button></div>
+            </div>
+        
        </el-card>
    </div>
    <div class = "col-sm-3">
-       <el-card style="margin-top:0px">
+       <el-card style="margin:0px 10px">
            <div slot="header" style="margin-left:6px">
                <span><i style="padding-right:6px" class="el-icon-video-camera"></i>视频录制</span>
            </div>
@@ -69,8 +75,68 @@ export default Vue.extend({
     
 }
 .stageCard{
-   .move{
+    position: relative;
+    left:400px;
+    bottom: 36px;
+    height: 140px;
+    width:200px;
+    cursor: pointer;
+    color: #999;
+   .movetop{
        font-size: 50px;
+       position: relative;
+       left:50px;
+       top:28px;
+       :hover{
+           color: #409EFF;
+       }
    }
+    .moveleft{
+       font-size: 50px;
+       position: relative;
+       left:0px;
+       :hover{
+           color: #409EFF;
+       }
+   }
+   .fBtn{
+       position: relative;
+       left:0px;
+       bottom: 13px;
+   }
+    .moveright{
+       font-size: 50px;
+       position: relative;
+       left:0px;
+       :hover{
+           color: #409EFF;
+       }
+   }
+    .movebottom{
+       font-size: 50px;
+       position: relative;
+       left:51px;
+       bottom:30px;
+       :hover{
+           color: #409EFF;
+       }
+   }
+}
+.general{
+    border: 1px solid #bbbbbb;
+    border-radius: 6px;
+    padding: 16px 16px 0px 16px;
+    padding-bottom: 0px;
+    position: relative;
+    margin-bottom: 16px;
+    margin-top: 6px;
+    font-size: 12px;
+    .title {
+        position: absolute;
+        top: -12px;
+        font-size: 14px;
+        background-color: #ffffff;
+        padding: 0 8px;
+    }
 }
 </style>
